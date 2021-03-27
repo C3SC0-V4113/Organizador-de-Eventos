891e26c2a249db04ef0eeea5dfed0179dbb8d273
Francisco Valle <vallecesco@gmail.com>
2021-03-27 03:27:34 -0600

    Base ActuaÃlizada

--- bd/mydb.sql
+++ bd/mydb.sql
@@ -3,11 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3307
--- Tiempo de generación: 21-03-2021 a las 20:48:04
+-- Tiempo de generación: 27-03-2021 a las 10:25:03
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,8 +39,7 @@ CREATE TABLE `detalle_empresa_enlaces` (
 INSERT INTO `detalle_empresa_enlaces` (`IDEnlaces`, `IDEmpresa`) VALUES
 (3, 1),
 (33, 1),
-(38, 1),
-(40, 1);
+(38, 1);
 
 -- --------------------------------------------------------
 
@@ -74,7 +71,7 @@ CREATE TABLE `empresa` (
 --
 
 INSERT INTO `empresa` (`idEmpresa`, `Nombre`, `TituloDescripcion`, `LogoDesc`, `Descripcion`, `EventosTitle`, `EventosDesc`, `UbicTitle`, `UbicDesc`, `ContactTitle`, `ContactDesc`, `Slogan`, `Telefono`, `Direccion`, `Email`, `Logo_Url`) VALUES
-(1, 'WINE & CHAMPAGNE EVENTOS', 'UNA EMPRESA GENIAL', 'fas fa-adjust', 'Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum ducimus sit impedit, consequuntur, enim cumque magnam doloribus laboriosam quidem alias, cum veniam ut deserunt. Culpa assumenda doloribus error perferendis dolor. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque dolores totam blanditiis temporibus, aut neque? Enim laboriosam vero molestiae voluptatum magni, exercitationem ducimus illum porro eaque sunt adipisci velit quasi.', '¡MOMENTOS INOLVIDABLES!', 'Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis numquam voluptatibus, sapiente sed sint enim deserunt omnis, necessitatibus impedit cupiditate est, magni tempore molestias repellat sequi ipsum error praesentium quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, obcaecati beatae commodi labore earum mollitia provident, rerum officia autem at, libero est in atque quia eaque minus quos ipsam! Aliquid! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia qui, dolorem aperiam quasi in, accusamus beatae vitae ducimus saepe asperiores sapiente possimus iste minus delectus ipsa maxime necessitatibus explicabo autem.', '¡ESTAMOS CERCA!!!!!!!', 'Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus voluptates est dolorem totam corrupti suscipit laudantium at, incidunt ipsa. Reprehenderit voluptates tempore similique illo hic nihil corrupti facilis, ad eius!\r\n\r\nEget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ad est porro minima nemo. Voluptatibus non architecto odit molestiae rem, sint eveniet veniam autem impedit ad inventore alias facere reiciendis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus incidunt natus magnam, blanditiis sequi eum ut recusandae sed hic voluptatibus neque tempora, numquam expedita aliquam enim molestias facilis corrupti dolore?', '¿Interesado?¡Contactanos!', 'Atenderemos en nuestras redes sociales', '¡B I E N V E N I D O S!', '78724055', 'Cima 4, Pasaje el Copinol, #173', 'vallecesco@gmail.com', '##');
+(1, 'WINE & CHAMPAGNE EVENTOS', 'UNA EMPRESA GENIAL', 'fas fa-ambulance', 'Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum ducimus sit impedit, consequuntur, enim cumque magnam doloribus laboriosam quidem alias, cum veniam ut deserunt. Culpa assumenda doloribus error perferendis dolor. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque dolores totam blanditiis temporibus, aut neque? Enim laboriosam vero molestiae voluptatum magni, exercitationem ducimus illum porro eaque sunt adipisci velit quasi.', '¡MOMENTOS INOLVIDABLES!', 'Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis numquam voluptatibus, sapiente sed sint enim deserunt omnis, necessitatibus impedit cupiditate est, magni tempore molestias repellat sequi ipsum error praesentium quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, obcaecati beatae commodi labore earum mollitia provident, rerum officia autem at, libero est in atque quia eaque minus quos ipsam! Aliquid! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia qui, dolorem aperiam quasi in, accusamus beatae vitae ducimus saepe asperiores sapiente possimus iste minus delectus ipsa maxime necessitatibus explicabo autem.', '¡ESTAMOS CERCA!!!!!!!', 'Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus voluptates est dolorem totam corrupti suscipit laudantium at, incidunt ipsa. Reprehenderit voluptates tempore similique illo hic nihil corrupti facilis, ad eius!\r\n\r\nEget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ad est porro minima nemo. Voluptatibus non architecto odit molestiae rem, sint eveniet veniam autem impedit ad inventore alias facere reiciendis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus incidunt natus magnam, blanditiis sequi eum ut recusandae sed hic voluptatibus neque tempora, numquam expedita aliquam enim molestias facilis corrupti dolore?', '¿Interesado?¡Contactanos!', 'Atenderemos en nuestras redes sociales', '¡B I E N V E N I D O S!', '78724055', 'Cima 4, Pasaje el Copinol, #173', 'vallecesco@gmail.com', '##');
 
 -- --------------------------------------------------------
 
@@ -95,8 +92,7 @@ CREATE TABLE `enlaces` (
 INSERT INTO `enlaces` (`IDEnlaces`, `Nombre`, `Enlace`) VALUES
 (3, 'Twitter', 'https://twitter.com/compose/tweet/media'),
 (33, 'GitHub', 'https://github.com/'),
-(38, 'Facebook', 'https://m.facebook.com/home.php'),
-(40, 'LinkedIn', 'https://www.linkedin.com/feed/');
+(38, 'Facebook', 'https://m.facebook.com/home.php');
 
 -- --------------------------------------------------------
 
@@ -116,7 +112,33 @@ CREATE TABLE `eventos` (
   `Descripcion` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `eventos`
+--
 
+INSERT INTO `eventos` (`idEventos`, `IdTipoEvento`, `IdEmpresa`, `Id_Usuario`, `Nombre`, `fecha`, `Lugar`, `Cliente`, `Descripcion`) VALUES
+(2, 3, 1, 1, 'Papyrus', '2016-06-27', 'Snowdin', 'Cool Dude', 'Veremos si esto es de su talla'),
+(4, 3, 1, 1, 'Cumpleaños', '2020-09-06', 'Pollo campero', 'Karla', 'Cumpleaños para toda la familia y Amigos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotoseventos`
+--
+
+CREATE TABLE `fotoseventos` (
+  `idFotos` int(11) UNSIGNED NOT NULL,
+  `idEventos` int(11) NOT NULL,
+  `UrlFoto` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `fotoseventos`
+--
+
+INSERT INTO `fotoseventos` (`idFotos`, `idEventos`, `UrlFoto`) VALUES
+(1, 2, 'ImagenesSubidas/3f8b4f4dcc6c8f205506cec38e95a48f1c8fc39b_hq.jpg'),
+(3, 4, 'ImagenesSubidas/fiesta-de-cumpleanos-655x368.jpg');
 
 -- --------------------------------------------------------
 
@@ -130,13 +152,14 @@ CREATE TABLE `infoservicios` (
   `DescripcionHeader` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `infoservicios`
+--
 
-CREATE TABLE `FotosEventos` (
-  `idFotos` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `idEventos` int(11) NOT NULL,
-  `UrlFoto`longtext DEFAULT NULL,
-  PRIMARY KEY (`idFotos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+INSERT INTO `infoservicios` (`idEmpresa`, `HeaderTitulo`, `DescripcionHeader`) VALUES
+(1, 'Nuestros bellos y rikonilos Servicios', 'Esto llegara a la historia');
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `servicios`
@@ -151,6 +174,14 @@ CREATE TABLE `servicios` (
   `Id_Usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`idServicios`, `Nombre`, `Descripcion`, `urlIMG`, `IdEmpresa`, `Id_Usuario`) VALUES
+(1, 'Probando los Servicios', 'Somos de una boda cuqui', 'fas fa-archive', 1, 1),
+(2, 'Segundo Servicio', 'Vamoooos', 'fas fa-ambulance', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -162,16 +193,19 @@ CREATE TABLE `tipoevento` (
   `Nombre` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO tipoevento (idTipoEvento,Nombre) VALUES
-(1,'Boda'),
-(2,'Entrega de Premios'),
-(3,'Fiestas'),
-(4,'Graduaciones'),
-(5,'Inaguraciones'),
-(6,'Ruedas de Presa'),
-(7,'Convenciones'),
-(8,'Eventos Deportivos');
+--
+-- Volcado de datos para la tabla `tipoevento`
+--
 
+INSERT INTO `tipoevento` (`idTipoEvento`, `Nombre`) VALUES
+(1, 'Boda'),
+(2, 'Entrega de Premios'),
+(3, 'Fiestas'),
+(4, 'Graduaciones'),
+(5, 'Inaguraciones'),
+(6, 'Ruedas de Presa'),
+(7, 'Convenciones'),
+(8, 'Eventos Deportivos');
 
 -- --------------------------------------------------------
 
@@ -184,6 +218,14 @@ CREATE TABLE `tipo_usuario` (
   `Tipo_Usuario` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`idTipo_Usuario`, `Tipo_Usuario`) VALUES
+(1, 'admin'),
+(2, 'cliente');
+
 -- --------------------------------------------------------
 
 --
@@ -191,11 +233,19 @@ CREATE TABLE `tipo_usuario` (
 --
 
 CREATE TABLE `usuario` (
-  `idUsuario` int(11),
+  `idUsuario` int(11) NOT NULL,
   `Usuario` varchar(45) DEFAULT NULL,
   `Id_tipo_usuario` int(11) NOT NULL,
-  `contraseña` varchar(20) DEFAULT NULL
+  `contraseña` varchar(400) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `Usuario`, `Id_tipo_usuario`, `contraseña`) VALUES
+(1, 'admin', 1, '98eb470b2b60482e259d28648895d9e1'),
+(3, 'cliente', 2, '89f624dc5f553676c8b05de3701e6178');
 
 --
 -- Índices para tablas volcadas
@@ -230,23 +280,20 @@ ALTER TABLE `eventos`
   ADD KEY `Id_Usuario` (`Id_Usuario`);
 
 --
+-- Indices de la tabla `fotoseventos`
+--
+ALTER TABLE `fotoseventos`
+  ADD PRIMARY KEY (`idFotos`),
+  ADD KEY `idEventos` (`idEventos`);
+
+--
 -- Indices de la tabla `infoservicios`
 --
 ALTER TABLE `infoservicios`
   ADD PRIMARY KEY (`idEmpresa`);
 
-  ALTER TABLE `FotosEventos`
-  ADD KEY `idEventos` (`idEventos`);
-
 --
 -- Indices de la tabla `servicios`
---
-
---
-
-
-
--- Estructura de tabla para la tabla `servicios`
 --
 ALTER TABLE `servicios`
   ADD PRIMARY KEY (`idServicios`),
@@ -283,6 +330,18 @@ ALTER TABLE `enlaces`
   MODIFY `IDEnlaces` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
+-- AUTO_INCREMENT de la tabla `fotoseventos`
+--
+ALTER TABLE `fotoseventos`
+  MODIFY `idFotos` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Restricciones para tablas volcadas
 --
 
@@ -302,15 +361,23 @@ ALTER TABLE `eventos`
   ADD CONSTRAINT `eventousuarios_ibfk_2` FOREIGN KEY (`Id_Usuario`) REFERENCES `usuario` (`idUsuario`);
 
 --
+-- Filtros para la tabla `fotoseventos`
+--
+ALTER TABLE `fotoseventos`
+  ADD CONSTRAINT `fotoe_ibfk_1` FOREIGN KEY (`idEventos`) REFERENCES `eventos` (`idEventos`);
+
+--
+-- Filtros para la tabla `infoservicios`
+--
+ALTER TABLE `infoservicios`
+  ADD CONSTRAINT `infoservicios_ibfk_1` FOREIGN KEY (`idEmpresa`) REFERENCES `empresa` (`idEmpresa`);
+
+--
 -- Filtros para la tabla `servicios`
 --
 ALTER TABLE `servicios`
   ADD CONSTRAINT `servicios_ibfk_1` FOREIGN KEY (`IdEmpresa`) REFERENCES `empresa` (`idEmpresa`),
   ADD CONSTRAINT `servicios_ibfk_2` FOREIGN KEY (`Id_Usuario`) REFERENCES `usuario` (`idUsuario`);
-
-ALTER TABLE `infoservicios`
-  ADD CONSTRAINT `infoservicios_ibfk_1` FOREIGN KEY (`IdEmpresa`) REFERENCES `empresa` (`idEmpresa`);
-
 
 --
 -- Filtros para la tabla `usuario`
@@ -319,10 +386,6 @@ ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`Id_tipo_usuario`) REFERENCES `tipo_usuario` (`idTipo_Usuario`);
 COMMIT;
 
-ALTER TABLE `FotosEventos`
-  ADD CONSTRAINT `fotoe_ibfk_1` FOREIGN KEY (`idEventos`) REFERENCES `eventos` (`idEventos`);
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-INSERT INTO tipo_usuario (idTipo_Usuario,Tipo_Usuario) values(1,'admin');
